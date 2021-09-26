764dfe12a75660649ac1fb795e520e4d544fc15e
Yeyoung Yu <79914479+uyeah0@users.noreply.github.com>
2021-09-26 19:57:36 +0900

    Add files via upload

---
+++ 변수의 유효범위와 클로저/LexicalEnvironment.js
@@ -0,0 +1,13 @@
+"use strict"
+function makeCounter() {
+    let count = 0;
+  
+    return function() {
+      return ++count;
+    };
+  }
+  
+let counter = makeCounter();
+console.log(counter());
+console.log(counter());
+console.log(counter());
---
+++ 재귀/factorial.js
@@ -0,0 +1,6 @@
+"use strict";
+
+function factorial(n){
+    return n ? n* factorial(n-1) : 1;
+}
+console.log(factorial(5));
