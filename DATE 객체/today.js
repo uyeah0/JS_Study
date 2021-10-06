76300d3dcf7665a5d36a890ec032dd393ebc8ccd
Yeyoung Yu <79914479+uyeah0@users.noreply.github.com>
2021-10-06 23:34:08 +0900

    Rename Date객체/today.js to DATE 객체/today.js

---
+++ DATE 객체/today.js
@@ -0,0 +1,5 @@
+"use strict";
+
+let now = new Date();
+console.log(now);
+console.log("오늘은 " + now.getFullYear() + "년 " + now.getMonth() + "월 " + now.getDate() + "일 입니다");
--- Date객체/today.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-let now = new Date();
-console.log(now);
-console.log("오늘은 " + now.getFullYear() + "년 " + now.getMonth() + "월 " + now.getDate() + "일 입니다");
